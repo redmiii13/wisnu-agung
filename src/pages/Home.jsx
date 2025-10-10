a6210677d1cbe08a2bf596163d97595dd3662fcf
redmiii13 <redmiii360@gmail.com>
2025-10-10 19:01:39 +0700

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -82,7 +82,7 @@ export default function Home() {
           </a>
 
           <a
-            href="https://drive.google.com/file/d/1JcFIfhkU-xXRkya2BNV5kmt7Us96pmHZ/view?usp=drivesdk"
+            href="https://drive.google.com/file/d/1lNijQg_SwHmaUTXze0GdoB5kypVEefNX/view?usp=sharing"
             className="w-48 h-9 rounded-full flex justify-center text-primary items-center border border-primary hover:border-primary hover:bg-primary hover:text-slate-900 transition duration-200"
             target="_blank"
             rel="noopener noreferrer"
